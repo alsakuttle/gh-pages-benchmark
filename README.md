542419845c1d46678e298cafefaf5eb026b6bfb6
uppedplays@gmail.com <uppedplays@gmail.com>
2022-04-30 12:40:24 +0800

    HOH

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# gh-pages-benchmark
+# PLChangelog 22410u1
+
+
+# Maintainers
+c4sc, khanhduytran0
+
+# Translating ⚠️
+## Crowdin
+[Crowdin](https://crowdin.com/project/PojavLauncher)
---
+++ assets/changelog/javascript-test/pircay.js
@@ -0,0 +1,6 @@
+script
+  while (true) {
+    alert("You’re using an old version of the launcher which is no longer available, please update to the latest.");
+    window.location.href = "https://github.com/PojavLauncherTeam/PojavLauncher/actions?query=is%3Asuccess+branch%3Av3_openjdk";
+  }
+script
